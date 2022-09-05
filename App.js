1ade59da3220ec23b6ef49d8b58c4f93b406771d
Kashyap2424 <kapdikashyap2003@gmail.com>
2022-09-05 15:52:45 +0530

    Learn core component of react native & how to style it.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyling}>Hello World!</Text>
+      <Text style={styles.textStyling}>Another Pices Of Text!</Text>
+      <Button title="Click Here !" />
     </View>
   );
 }
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyling: {
+    margin: 16,
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 16,
   },
 });
